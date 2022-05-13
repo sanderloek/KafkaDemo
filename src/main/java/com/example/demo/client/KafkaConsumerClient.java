38c3ffe8cfac72e2d98e68c57df34d33aac97466
sanderloek <sander.loek.peeters@gmail.com>
2022-05-13 20:17:07 +0200

    MVP

--- src/main/java/com/example/demo/client/KafkaConsumerClient.java
+++ src/main/java/com/example/demo/client/KafkaConsumerClient.java
@@ -11,7 +11,6 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
-import static java.util.UUID.randomUUID;
 
 public class KafkaConsumerClient {
 
@@ -21,43 +20,41 @@ public class KafkaConsumerClient {
     public KafkaConsumerClient() {
         props = new Properties();
         props.setProperty("bootstrap.servers", "localhost:9092");
-        props.setProperty("group.id", randomUUID().toString());
+        props.setProperty("group.id", "test-group");
         props.setProperty("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         props.setProperty("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
         consumer = new KafkaConsumer<>(props);
     }
 
     public ResponseEntity readMessage(String topic, int limit) {
         consumer.subscribe(Collections.singleton(topic));
 
-        ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+        ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
 
         int skip = getSkipNumber(limit, records.count());
 
-        List<ConsumerRecord<String, String>> result =
+        List<String> messages =
                 StreamSupport.stream(records.records(topic).spliterator(), false)
                         .skip(skip)
+                        .map(ConsumerRecord::value)
                         .collect(Collectors.toList());
 
-
-
-        if (result.size() == 0)
-            return new ResponseEntity<>("No Messages Found for " + topic  ,HttpStatus.INTERNAL_SERVER_ERROR);
+        if (messages.size() == 0)
+            return new ResponseEntity<>("No New Messages Found for topic: " + topic, HttpStatus.INTERNAL_SERVER_ERROR);
         else
-            return new ResponseEntity<>("Messages for " + topic + result, HttpStatus.OK);
+            return new ResponseEntity<>("Messages for topic: " + topic + messages, HttpStatus.OK);
+
 
     }
 
     private int getSkipNumber (int limit, int count)
     {
         if (limit > count)
-        {
             return 0;
-        }
         else
-        {
             return count - limit;
-        }
+
     }
 
 }
